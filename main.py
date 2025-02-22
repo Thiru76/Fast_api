ea99865c7d369f46aff61618563cf2f382cccef7
Thiru76 <dineshdin7699@gmail.com>
2025-02-22 22:32:39 +0530

    till delete completed

--- main.py
+++ main.py
@@ -44,6 +44,14 @@ def find_post(id):
             print(i)
             return i
 
+
+def finding_index(id):
+    for index, i in enumerate(empty_list):
+        if i['id'] == id:
+            return index
+
+
+
 @app.get("/get_specific_post/{id}")
 def get_specific_post(id : int, response : Response):
     print(id)
@@ -52,3 +60,14 @@ def get_specific_post(id : int, response : Response):
         response.status_code = status.HTTP_404_NOT_FOUND
         return {"message": f"this {id} is not found"}
     return retreived_post
+
+@app.delete("/delete_post/{id}")
+def delete_post(id : int):
+    print(id)
+    index = finding_index(id)
+    if index == None:
+        return Response(status_code=status.HTTP_404_NOT_FOUND, detail ="The given {id} is not available")
+    
+    empty_list.pop(index)
+    return Response(status_code=status.HTTP_200_OK)
+
