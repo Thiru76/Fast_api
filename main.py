a480cf6aa502f0a91fb6eeb9e1cbc272f43ffc6f
Thiru76 <dineshdin7699@gmail.com>
2025-02-23 14:57:53 +0530

    CRUD completed

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from fastapi import Response, status, HTTPException
 
 app = FastAPI()
 
-
+empty_list = [{'title': 'about a man', 'content': 'Thiru is a good boy', 'published': False, 'id': 79},{'title': 'man_1', 'content': 'Thiru is a good boy', 'published': False, 'id': 77}]
 #This is the model from pydantic which helps ue to validate the input what user is providing
 class Post(BaseModel): 
     title : str
@@ -20,22 +20,22 @@ def sample():
 
 
 #it will get the body from the uer input and save that as dictionary
-@app.post("/create_post")
-def create_post(info : dict = Body()):
-    return(info)
+# @app.post("/create_post")
+# def create_post(info : dict = Body()):
+#     return(info)
 
-empty_list = [{'title': 'about a man', 'content': 'Thiru is a good boy', 'published': False, 'id': 79},{'title': 'man_1', 'content': 'Thiru is a good boy', 'published': False, 'id': 77}]
+
 
 #WE called the class called Post adn saved to variable adn asking them to print and return it will validate for us
-@app.post("/using_pydantic")
+@app.post("/create_post")
 def create_post(value: Post): # type: ignore
     # print(value)
     # print(value.content)
     value = value.dict()
     value['id']= randrange(0, 100000)
-    empty_list.append(value)
-    print(empty_list)
-    return(value)
+    output = empty_list.append(value)
+    print(output)
+    return(output)
 
 def find_post(id):
     for i in empty_list:
@@ -71,3 +71,16 @@ def delete_post(id : int):
     empty_list.pop(index)
     return Response(status_code=status.HTTP_200_OK)
 
+@app.put("/update_post/{id}")
+def update_post(id: int, update_post : Post):
+    print("this is the update function")
+    index = finding_index(id)
+    print(index)
+    # if index == None:
+    #     return Response(status_code=status.HTTP_404_NOT_FOUND, detail ="The given {id} is not available")
+    
+
+    post_dict = update_post.dict()
+    post_dict['id'] = id
+    empty_list[index] = post_dict 
+    return {"mesg": " this is update func"}
